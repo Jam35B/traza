1034e298f1678caac021c9037b327651da5d5759
Jose Luis Saavedra <103464355+Jam35B@users.noreply.github.com>
2024-10-07 20:41:34 -0300

    fix

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,112 +1,96 @@
 <template>
-  <!-- Se define la estructura de la página, usando Quasar's q-page para el diseño. -->
   <q-page>
-    <!-- Título centrado de la aplicación. -->
     <p class="text-h6 text-center">Traza</p>
-    <!-- Botón centrado que inicia la lectura de códigos de barras al hacer clic. -->
+
     <div class="text-center">
       <q-btn
         label="Iniciar Lectura"
         color="primary"
         icon="mdi-barcode-scan"
         size="lg"
-        @click="initReader" 
-      /> 
-    </div> <!-- Evento que llama a la función initReader al hacer clic. -->
-
-    <!-- Muestra el código de barras leído si la variable 'code' tiene un valor. -->
+        @click="initReader"
+      />
+    </div>
     <p
       v-if="code"
-      class="text-h6 text-center q-mt-lg"> <!-- Condición que muestra el código solo si ya fue escaneado. -->
-      Código Leido: {{ code }} <!-- Muestra el código leído. -->
-
-      <!-- Botón que copia el código de barras leído al portapapeles. -->
+      class="text-h6 text-center q-mt-lg">
+      Código Leido: {{ code }}
       <q-btn
         icon="mdi-content-copy"
-        @click="copyCode" 
+        @click="copyCode"
         size="md"
         dense
         color="blue-9"
-      /> <!-- Llama a la función copyCode al hacer clic. -->
+      />
     </p>
-
-    <!-- Div que muestra el lector de la cámara cuando 'cameraStatus' es verdadero. -->
     <div
       class="text-center"
-      v-show="cameraStatus" 
-      id="reader"></div> <!-- Controla la visibilidad según el estado de la cámara y define el area donde se visualiza el flujo de la camara -->
+      v-show="cameraStatus"
+      id="reader"></div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue' // Importa Vue para definir componentes y crear referencias reactivas.
-import Quagga from 'quagga' // Importa la librería Quagga para escanear códigos de barras.
-import { copyToClipboard, useQuasar } from 'quasar' // Importa utilidades de Quasar para copiar al portapapeles y notificaciones.
+import { defineComponent, ref } from 'vue'
+import Quagga from 'quagga'
+import { copyToClipboard, useQuasar } from 'quasar'
 
 export default defineComponent({
-  name: 'IndexPage', // Nombre del componente.
+  name: 'IndexPage',
   setup () {
-    // Definición de variables reactivas.
-    const code = ref('') // Almacena el código escaneado.
-    const cameraStatus = ref(false) // Controla el estado de la cámara (activo o inactivo).
-    const q$ = useQuasar() // Acceso a las funcionalidades de Quasar, como notificaciones.
-
-    // Función que inicializa el lector de códigos de barras usando Quagga.
+    const code = ref('')
+    const cameraStatus = ref(false)
+    const q$ = useQuasar()
     const initReader = () => {
-      cameraStatus.value = true // Activa la cámara.
+      cameraStatus.value = true
       Quagga.init({
         inputStream: {
-          name: 'Live', // Fuente de entrada en vivo (cámara).
+          name: 'Live',
           type: 'LiveStream',
-          target: document.querySelector('#reader') // El elemento donde se mostrará la transmisión de la cámara.
+          target: document.querySelector('#reader')
         },
         decoder: {
-          readers: ['ean_reader'] // Especifica el tipo de códigos de barras a leer (EAN).
+          readers: ['ean_reader']
         }
       }, function (err) {
         if (err) {
-          console.log(err) // Muestra errores en la consola.
+          console.log(err)
           return
         }
         console.log('Initialization finished. Ready to start')
-        Quagga.start() // Inicia el escaneo.
+        Quagga.start()
         Quagga.onDetected((data) => {
-          onDetected(data) // Llama a la función onDetected cuando se detecta un código.
+          onDetected(data)
         })
       })
     }
 
-    // Función que detiene el lector de códigos de barras.
     const stopReader = () => {
-      cameraStatus.value = false // Desactiva la cámara.
-      Quagga.stop() // Detiene el lector.
+      cameraStatus.value = false
+      Quagga.stop()
     }
 
-    // Función que se ejecuta cuando se detecta un código de barras.
     const onDetected = (data) => {
-      code.value = data.codeResult.code // Asigna el código escaneado a la variable 'code'.
-      stopReader() // Detiene el lector tras detectar un código.
-      console.log(data) // Muestra los datos detectados en la consola.
+      code.value = data.codeResult.code
+      stopReader()
+      console.log(data)
     }
 
-    // Función para copiar el código escaneado al portapapeles.
     const copyCode = () => {
-      copyToClipboard(code.value) // Copia el valor de 'code' al portapapeles.
+      copyToClipboard(code.value)
         .then(() => {
           q$.notify({
-            message: 'Código de barras copiado con éxito!', // Notificación de éxito.
-            type: 'positivo'
+            message: 'Código de barras copiado con éxito!',
+            type: 'positive'
           })
         })
         .catch(() => {
           q$.notify({
-            message: 'error al copiar el codigo de barras!', // Notificación de error.
+            message: 'Error al copiar código de barras!',
             type: 'negative'
           })
         })
     }
-
-    // Retorno de las funciones y variables para que estén disponibles en el template.
     return {
       initReader,
       stopReader,
